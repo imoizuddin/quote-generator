ae79197cefb174c8e6b84eb2a260f290f9545e29
Moizuddin Qureshi <imoizuddin@gmail.com>
2020-12-25 01:34:32 +0530

    Fetching Quotes from API

---
+++ script.js
@@ -0,0 +1,46 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+//Get Quote from API
+async function getQuote() {
+  const proxyUrl = "https://whispering-tor-04671.herokuapp.com/";
+  const API =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + API);
+    const data = await response.json();
+    //if author blank replace with Unknown
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    //Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
+  } catch (error) {
+    getQuote();
+    console.log("Error", error);
+  }
+}
+
+//Tweeet Quote
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl,'_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click',getQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
+//On Load
+getQuote();
