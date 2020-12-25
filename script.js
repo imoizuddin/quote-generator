216faa1d47da81a4d6e9efdb058c4e3da66d4c40
Moizuddin Qureshi <imoizuddin@gmail.com>
2020-12-25 09:54:13 +0530

    Added a loader animation

--- script.js
+++ script.js
@@ -3,8 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+//Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
 //Get Quote from API
 async function getQuote() {
+  loading();
   const proxyUrl = "https://whispering-tor-04671.herokuapp.com/";
   const API =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
@@ -24,6 +41,8 @@ async function getQuote() {
       quoteText.classList.remove("long-quote");
     }
     quoteText.innerText = data.quoteText;
+    //Stop Loader, Show Quote
+    complete();
   } catch (error) {
     getQuote();
     console.log("Error", error);
@@ -31,16 +50,17 @@ async function getQuote() {
 }
 
 //Tweeet Quote
-function tweetQuote(){
-    const quote = quoteText.innerText;
-    const author = authorText.innerText;
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-    window.open(twitterUrl,'_blank');
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
 }
 
 //Event Listeners
-newQuoteBtn.addEventListener('click',getQuote);
-twitterBtn.addEventListener('click',tweetQuote);
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 //On Load
 getQuote();
+// loading();
